5eb43799e90836b7cefde49ad2c3da68f5a7bff1
frantisek.hallo <fantomx1@gmail.com>
2020-08-10 15:04:09 +0200

    view locator trait adjusted for polymorphism

--- ViewLocatorRenderTrait.php
+++ ViewLocatorRenderTrait.php
@@ -13,15 +13,33 @@ trait ViewLocatorRenderTrait
 
 
     /**
+     * @return string
+     */
+    protected function getDefaultViewsDir($dir = '')
+    {
+        return $dir;
+    }
+
+
+    /**
+    * protected function getViewsDir()
+    * {
+    * return $this->getDefaultViewsDir();
+    * }
+    * @return string
+    */
+    abstract protected function getViewsDir();
+
+    /**
      * @param $template
      * @param $vars
      */
-    private function render($template, $vars)
+    protected function render($template, $vars)
     {
         $tl = new ViewLocator();
 
-        if (!empty($this->viewsDir)) {
-            $tl->setViewsDir($this->viewsDir);
+        if ($viewsDir = $this->getViewsDir()) {
+            $tl->setViewsDir($viewsDir);
         }
 
         $path = $tl->seek($this);
